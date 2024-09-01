619b343a5c77b233c04a83dc3fe651739bdecb87
gszkopinski <gszkopinski@gmail.com>
2024-09-01 11:15:27 +0200

    fix(items): Wrong url

--- artifactsmmo_sdk/items/items.py
+++ artifactsmmo_sdk/items/items.py
@@ -64,7 +64,7 @@ class Items:
         page: Annotated[int, Field(description="Page number.", ge=1, default=1)] = 1,
         size: Annotated[int, Field(description="Page size.", ge=1, le=100, default=50)] = 50,
     ) -> Tuple[str, ListItemsResponseSchema | None]:
-        """Fetch monsters details."""
+        """Fetch items details."""
         try:
             parameters = f"page={page}"
             parameters += f"&size={size}"
@@ -76,13 +76,13 @@ class Items:
             parameters += f"&name={name}" if name else ""
 
             response = self.session.get(
-                url=f"{self.api_url}/monsters?{parameters}",
+                url=f"{self.api_url}/items?{parameters}",
             )
 
             response.raise_for_status()
 
             return (
-                "Successfully fetched monsters details.",
+                "Fetch items details.",
                 ListItemsResponseSchema.model_validate(response.json())
             )
 
